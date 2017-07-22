df8c977f368c2e1bc313e0a73364a69093640a4e
Christopher Lowenthal <clowenhg@gmail.com>
2017-07-22 17:39:17 -0400

    Made config file selectable via cli

--- src/odyssey.ts
+++ src/odyssey.ts
@@ -1,21 +1,26 @@
 #!/usr/bin/env node
 
 import * as fs from "fs";
+import * as Commander from "commander";
 import { OdysseyServer } from "./server/server";
 import { AdminServer } from './server/admin/admin-server';
 import { GameState } from './server/game-state';
 
+Commander
+  .option('-c, --config [config]', 'Configuration File')
+  .parse(process.argv);
+
+let configFile = Commander.config || 'config/odyssey.config.json';
+
 let config: Odyssey.Config;
 try {
-  config = JSON.parse(fs.readFileSync('odyssey.config.json', 'utf-8'));
+  config = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
 } catch (ex) {
-  console.error('Unable to read odyssey.config.json');
+  console.error(`Unable to read ${configFile}`);
   console.error(ex);
   process.exit(-1);
 }
 
 let gameState = new GameState(config);
-this.gameState.start();
-
 let server = new OdysseyServer(gameState, config);
 let adminServer = new AdminServer(gameState, config);
