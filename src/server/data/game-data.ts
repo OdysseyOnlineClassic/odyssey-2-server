78a41f08b61cf55816b8dce7bc6b776f9a271cfb
Christopher Lowenthal <clowenhg@gmail.com>
2017-09-05 07:45:36 -0400

    Removed Active Record pattern Kept the private constructor and ability to call `save()` on a document.

--- src/server/data/game-data.ts
+++ src/server/data/game-data.ts
@@ -1,6 +1,6 @@
 import * as NeDB from 'nedb';
 
-export class GameDataManager<T extends GameDataDocument> {
+export class GameDataManager<T extends Server.GameDataDocument> {
   protected data: NeDB;
 
   constructor(dataFile: string) {
@@ -39,155 +39,20 @@ export class GameDataManager<T extends GameDataDocument> {
  * @export
  * @class DataDocument
  */
-@data
-export abstract class DataDocument {
-  readonly _id: string;
-  clearChanges() { };
-  save() { };
-}
+export abstract class DataDocument implements Server.DataDocument {
+  constructor(
+    protected data: NeDB,
+    public readonly _id: string, )
+  { }
 
-/**
- * Represents game data that's indexed numerically and versioned to reduce load times
- *
- * @export
- * @interface GameDataDocument
- * @extends {DataDocument}
- */
-export interface GameDataDocument {
-  index: number;
-  version: number;
-}
-
-export function data(target: any) {
-  target.prototype._autoSave = false;
-  target.prototype._changedKeys = [];
-  target.prototype._trackedKeys = target.prototype._trackedKeys || {};
-
-  target.prototype.clearChanges = function clearChanges() {
-    this._changedKeys = [];
-  }
-
-  target.prototype.getSets = function getSets() {
-    let set = {};
-    let key;
-    for (let i = 0; i < this._changedKeys.length; i++) {
-      key = this._changedKeys[i];
-      set[key] = this[key];
-    }
-
-    return set;
-  }
-
-  target.prototype.markChanged = async function markChanged(key) {
-    let save = this._trackedKeys[key].autoSave;
-    if (save == false) {
-      this._changedKeys.push(key);
-      return Promise.resolve();
-    } else if (save === true) {
-      let set = {}
-      set[key] = this[key];
-      this.data.update({ _id: this._id }, { $set: { set } }, {}, (err, numAffected) => {
-        if (err) {
-          return Promise.reject(err);
-        }
-
-        if (numAffected != 1) {
-          return Promise.reject(`Save Changes number affected != 1 (${numAffected})`);
-        }
-        return Promise.resolve();
-      });
-    }
-  }
-
-  target.prototype.save = function saveChanges() {
-    return new Promise((resolve, reject) => {
-      let sets = this.getSets();
-      this.data.update({ _id: this._id }, { $set: sets }, {}, (err, numAffected) => {
+  public async save(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.data.update({ _id: this._id }, this, {}, (err, numUpdated) => {
         if (err) {
           return reject(err);
         }
-
-        if (numAffected != 1) {
-          return reject(`Save Changes number affected != 1 (${numAffected})`);
-        }
-        this.clearChanges();
-        resolve(this);
+        resolve();
       });
     });
   }
-}
-
-export function trackProperty(autoSave: boolean = true) {
-  return function track(target: any, key: string) {
-    let trackedKeys = target.constructor.prototype._trackedKeys || {};
-    trackedKeys[key] = {
-      autoSave: (autoSave == true)
-    }
-
-    let _val = target[key];
-
-    let getter = function () {
-      return _val;
-    };
-
-    let setter = function (newValue) {
-      if (newValue != null && typeof newValue === 'object') {
-        //Have to handle arrays and objects
-        if (Array.isArray(newValue)) {
-
-        } else {
-          let props = Object.getOwnPropertyNames(newValue);
-          for (let i = 0; i < props.length; i++) {
-            let propValue;
-            let prop = props[i];
-            console.log(prop);
-            let descriptor = Object.getOwnPropertyDescriptor(newValue, prop);
-            let dataObj = this;
-
-            if (descriptor.set) {
-              descriptor.set = function (val) {
-                dataObj.markChanged(key);
-                descriptor.set(val);
-              }
-            } else {
-              descriptor.set = function (val) {
-                dataObj.markChanged(key);
-                propValue = val;
-              }
-            }
-
-            if (!descriptor.get) {
-              descriptor.get = function () {
-                return propValue;
-              }
-            }
-
-            delete (descriptor.writable);
-            delete (descriptor.value);
-
-            Object.defineProperty(newValue, prop, descriptor);
-          }
-        }
-      }
-      _val = newValue;
-      this.markChanged(key);
-    }
-
-    if (delete target[key]) {
-      Object.defineProperty(target, key, {
-        get: getter,
-        set: setter,
-        enumerable: true,
-        configurable: true
-      });
-    }
-  }
-}
-
-function arraySetter(arr, autoSave: boolean) {
-
-}
-
-function objectSetter(obj, autoSave: boolean) {
-
 }
--- tests/unit/server/data/accounts.spec.ts
+++ tests/unit/server/data/accounts.spec.ts
@@ -19,76 +19,12 @@ describe('AccountDataManager', () => {
     data = new AccountDataManager(dataFile);
   });
 
-  it('returns an AccountDocument', async () => {
+  it('should return an AccountDocument', async () => {
     let account = await data.createAccount('test', 'password', 'email');
     expect(account).instanceof(AccountDocument);
   });
 
   describe('AccountDocument', () => {
-    describe('Data Decorator', () => {
-      let subject: any; //We use subject because TypeScript doesn't know about decorator properties
 
-      before(async () => {
-        let account = await data.createAccount('data-decorator', 'password', 'email');
-        subject = account as any;
-        subject.saveChanges = sinon.spy();
-        account.access = 3;
-      });
-
-      describe('_changedKeys', () => {
-        it('should exist on account', () => {
-          expect(subject).to.haveOwnProperty('_changedKeys');
-        });
-
-        it('should not be updated on set (all properties are autosave)', () => {
-          expect(subject._changedKeys).to.have.lengthOf(0);
-        });
-      });
-
-      describe('saveChanges', () => {
-        it('should have saveChanges', () => {
-          expect(subject.saveChanges).to.be.a('Function');
-        })
-
-        it('should call saveChanges', () => {
-          expect(subject.saveChanges).to.have.been.calledOnce;
-        });
-      });
-    });
-
-    describe('Auto Save', () => {
-      let subject: any; //We use subject because TypeScript doesn't know about decorator properties
-
-      before(async () => {
-        let account = await data.createAccount('username', 'password', 'email');
-        subject = account as any;
-        subject.saveChanges = sinon.spy();
-        return;
-      });
-
-      beforeEach(() => {
-        subject.saveChanges.reset();
-      });
-
-      it('should auto save access', () => {
-        subject.access = 10;
-        expect(subject.saveChanges).to.have.been.calledOnce;
-      });
-
-      it('should auto save email', () => {
-        subject.email = 'test email';
-        expect(subject.saveChanges).to.have.been.calledOnce;
-      });
-
-      it('should auto save password', () => {
-        subject.password = 'new-password';
-        expect(subject.saveChanges).to.have.been.calledOnce;
-      });
-
-      it('should auto save username', () => {
-        subject.username = 'new-username';
-        expect(subject.saveChanges).to.have.been.calledOnce;
-      });
-    });
   });
 })
